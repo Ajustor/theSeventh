2c39bf3080b0796ef411204bd14ea802a6b9d778
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2026-01-16 11:40:38 +0100

    feat(door_management): start adding lever system and door management

--- src/world/objects.rs
+++ src/world/objects.rs
@@ -17,6 +17,10 @@ pub struct DoorBundle {
     pub sprite_sheet: Sprite,
     #[from_entity_instance]
     pub collider_bundle: ColliderBundle,
+    // #[from_entity_instance]
+    // pub locked_by: EntityRef,
+    #[from_entity_instance]
+    pub locked: bool,
 }
 
 #[derive(Clone, Default, Bundle, LdtkEntity)]
@@ -25,12 +29,21 @@ pub struct PumpkinsBundle {
     pub sprite_sheet: Sprite,
 }
 
+#[derive(Clone, Default, Bundle, LdtkEntity)]
+pub struct LeverBundle {
+    #[sprite_sheet(no_grid)]
+    pub sprite_sheet: Sprite,
+
+    pub state: bool,
+}
+
 pub struct MiscObjectsPlugin;
 
 impl Plugin for MiscObjectsPlugin {
     fn build(&self, app: &mut App) {
         app.register_ldtk_entity::<ChestBundle>("Chest")
             .register_ldtk_entity::<DoorBundle>("Door")
-            .register_ldtk_entity::<PumpkinsBundle>("Pumpkins");
+            .register_ldtk_entity::<PumpkinsBundle>("Pumpkins")
+            .register_ldtk_entity::<LeverBundle>("Lever");
     }
 }
